5c23a1c4ac89588cc86754b9a06ce0886536c9a8
Nathen <ww18256962582@163.com>
2020-05-05 15:01:15 +0800

    创建service

---
+++ src/main/java/com/webServer/project/service/impl/UserServiceImpl.java
@@ -0,0 +1,11 @@
+package com.webServer.project.service.impl;
+
+import com.webServer.project.service.UserService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class UserServiceImpl implements UserService {
+
+}
