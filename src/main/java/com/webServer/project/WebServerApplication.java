cd040a5bf1c7eae541b8ea8ee6d5d709f6749801
json <json@guigu.com>
2020-05-05 13:25:24 +0800

    springboot扫描包路径

--- src/main/java/com/webServer/project/WebServerApplication.java
+++ src/main/java/com/webServer/project/WebServerApplication.java
@@ -1,8 +1,10 @@
 package com.webServer.project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.webServer.project")
 public class WebServerApplication {
 
     public static void main(String[] args) {
