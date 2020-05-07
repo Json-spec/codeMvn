da274888c09e1d985ef6675dff67b1af08ff5ade
admin <1821118219@qq.com>
2020-05-07 14:53:03 +0800

    123

--- src/main/java/com/webServer/project/WebServerApplication.java
+++ src/main/java/com/webServer/project/WebServerApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @ComponentScan("com.webServer.project")
@@ -15,9 +15,9 @@ public class WebServerApplication {
         SpringApplication.run(WebServerApplication.class, args);
     }
 
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder(){
-        return new BCryptPasswordEncoder();
-    }
+//    @Bean
+//    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+//        return new BCryptPasswordEncoder();
+//    }
 
 }
--- src/main/java/com/webServer/project/controller/UserController.java
+++ src/main/java/com/webServer/project/controller/UserController.java
@@ -1,11 +1,17 @@
 package com.webServer.project.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/user")
+@Controller
 public class UserController {
-
+    @RequestMapping("index")
+    public String toindex(){
+        return "index";
+    }
+    @RequestMapping("login")
+    public String tologin(){
+        return "register";
+    }
 }
